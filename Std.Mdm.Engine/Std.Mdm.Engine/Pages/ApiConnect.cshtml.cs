ab7b095985524df8d1a3653af252547b8dcfda5e
Lőrincz Csaba <lorincz.csaba99@gmail.com>
2020-08-26 16:17:24 +0300

    Connection in progress

---
+++ Std.Mdm.Engine/Std.Mdm.Engine/Pages/ApiConnect.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Std.Mdm.Engine.Pages
+{
+    public class ApiConnectModel : PageModel
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+           // client.DefaultRequestHeaders.Accept.Add(
+              //  new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+           // client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("http://localhost/hmdm");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+       
+        public async void OnGet()
+        {
+            await ProcessRepositories();
+
+        }
+    }
+}
